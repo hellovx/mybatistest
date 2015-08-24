236b27c12be2fd9faf889b31ec4ad7c5563e5f8d
hellovx <422789400@qq.com>
2015-08-24 17:30:04 +0800

    springmvc+mybatis 接口开发junit测试可以实现了查询users表了（也就是说mybatis正式加入到框架中）

--- mybatistest/src/main/java/com/zh/controller/test/TestController.java
+++ mybatistest/src/main/java/com/zh/controller/test/TestController.java
@@ -1,19 +1,32 @@
 package com.zh.controller.test;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.zh.po.test.Users;
+import com.zh.service.test.UsersService;
 
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 	
+	@Autowired
+	private UsersService usersService;
+	
 	@RequestMapping("/hello.do")
 	public String test(Model model,@RequestParam("username") String name){
 		System.out.println("init test/hello.do");
 		System.out.println("name:"+name);
+		
+		List<Users> list=this.usersService.getUsersList();
+		
+		
 		model.addAttribute("name", name);
 		return "list";
 	}
